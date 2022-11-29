c85c6e52aa7d64721a648bf8d29ccba31b5917c0
Timur <48119843+timamus@users.noreply.github.com>
2022-11-29 18:20:27 +0600

    Update 3_install_main_apps.sh

--- 3_install_main_apps.sh
+++ 3_install_main_apps.sh
@@ -7,5 +7,14 @@ echo -en "\033[1;33m Installing veracrypt... \033[0m \n"
 sudo add-apt-repository ppa:unit193/encryption
 sudo apt update && sudo apt install veracrypt
 
+# Installing virtualbox
+echo -en "\033[1;33m Installing virtualbox... \033[0m \n"
+sudo apt install virtualbox virtualbox-ext-pack
+sudo gpasswd -a $USER vboxusers
+
+# Installing remmina with rdp and vnc plugins
+echo -en "\033[1;33m Installing remmina with rdp and vnc plugins... \033[0m \n"
+sudo apt install remmina remmina-plugin-rdp remmina-plugin-vnc remmina-plugin-secret
+
 echo -en "\033[0;35m Installation successfull \033[0m \n"
 echo 'A system reboot is recommended. Reboot? (y/n)' && read x && [[ "$x" == "y" ]] && /sbin/reboot;
