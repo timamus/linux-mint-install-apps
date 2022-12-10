622a2b6374e188b444d81eb0ad0952d39a1162f6
Timur <48119843+timamus@users.noreply.github.com>
2022-12-10 15:34:48 +0600

    Update 3_install_main_apps.sh

--- 3_install_main_apps.sh
+++ 3_install_main_apps.sh
@@ -120,6 +120,10 @@ echo -en "\033[1;33m Installing ProtonVPN from AUR... \033[0m \n"
 URL=$'https://repo.protonvpn.com/debian/dists/stable/main/binary-all/'
 BINARY_FILE=$(wget -qO- $URL | grep -oP 'href="\Kprotonvpn-stable-release.+?deb')
 URL="${URL}${BINARY_FILE}"
+wget $URL
+sudo dpkg -i $BINARY_FILE
+rm -f $BINARY_FILE
+sudo apt update && sudo apt install protonvpn
 
 echo -en "\033[0;35m Installation successfull \033[0m \n"
 echo 'A system reboot is recommended. Reboot? (y/n)' && read x && [[ "$x" == "y" ]] && /sbin/reboot;
