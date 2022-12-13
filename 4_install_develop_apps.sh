082ba51d5d9f417974399a794474ff16ef8f8168
Timur <48119843+timamus@users.noreply.github.com>
2022-12-13 12:10:07 +0600

    Update 4_install_develop_apps.sh

--- 4_install_develop_apps.sh
+++ 4_install_develop_apps.sh
@@ -1,1 +1,17 @@
+#!/usr/bin/env bash
 
+set -Eeuo pipefail
+
+# Installing visual studio code
+echo -en "\033[1;33m Installing visual studio code... \033[0m \n"
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+
+sudo apt install -y apt-transport-https
+sudo apt update
+sudo apt install -y code # or code-insiders
+
+echo -en "\033[0;35m Installation successfull \033[0m \n"
+echo 'A system reboot is recommended. Reboot? (y/n)' && read x && [[ "$x" == "y" ]] && /sbin/reboot;
