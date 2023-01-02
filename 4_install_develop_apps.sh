de95cdf8652c1fc7415a46b63e7d80065e8c5593
Timur <48119843+timamus@users.noreply.github.com>
2023-01-02 11:52:31 +0600

    Update 4_install_develop_apps.sh

--- 4_install_develop_apps.sh
+++ 4_install_develop_apps.sh
@@ -21,6 +21,32 @@ chmod +x ./dotnet-install.sh
 ./dotnet-install.sh -c 6.0 --runtime aspnetcore
 echo 'export PATH=$PATH:/home/`whoami`/.dotnet' >> ~/.bashrc
 
+# Installing jetbrains-toolbox
+echo -en "\033[1;33m Installing jetbrains-toolbox... \033[0m \n"
+function getLatestUrl() {
+USER_AGENT=('User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36')
+URL=$(curl 'https://data.services.jetbrains.com/products/releases?code=TBA&latest=true&type=release' -H 'Origin: https://www.jetbrains.com' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-US,en;q=0.8' -H "${USER_AGENT[@]}" -H 'Accept: application/json, text/javascript, */*; q=0.01' -H 'Referer: https://www.jetbrains.com/toolbox/download/' -H 'Connection: keep-alive' -H 'DNT: 1' --compressed | grep -Po '"linux":.*?[^\\]",' | awk -F ':' '{print $3,":"$4}'| sed 's/[", ]//g')
+echo $URL
+}
+getLatestUrl
+
+FILE=$(basename ${URL})
+DEST=$PWD/$FILE
+
+wget -cO  ${DEST} ${URL} --read-timeout=5 --tries=0
+DIR="/opt/jetbrains-toolbox"
+if mkdir ${DIR}; then
+    tar -xzf ${DEST} -C ${DIR} --strip-components=1
+fi
+chmod -R +rwx ${DIR}
+
+ln -s ${DIR}/jetbrains-toolbox /usr/local/bin/jetbrains-toolbox
+chmod -R +rwx /usr/local/bin/jetbrains-toolbox
+rm ${DEST}
+
+sudo sed -i '/1.2.3.4 account.jetbrains.com/d' /etc/hosts
+sudo bash -c "echo 1.2.3.4 account.jetbrains.com >> /etc/hosts"
+
 # Installing visual studio code
 echo -en "\033[1;33m Installing visual studio code... \033[0m \n"
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
