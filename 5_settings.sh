9af45e9793da864fc539ce35c97df45fb1332b9e
Timur <48119843+timamus@users.noreply.github.com>
2023-01-06 13:30:09 +0600

    Update 5_settings.sh

--- 5_settings.sh
+++ 5_settings.sh
@@ -107,5 +107,9 @@ else
   echo -en "\033[0;31m Cancelled! The timeshift program was not installed... \033[0m \n"
 fi
 
+# Installing recommended drivers
+echo -en "\033[1;33m Install the recommended drivers. After installation, close the Driver Manager and the configuration script will continue... \033[0m \n"
+sudo mintdrivers
+
 echo -en "\033[0;35m System settings are completed \033[0m \n"
 echo 'A system reboot is recommended. Reboot? (y/n)' && read x && [[ "$x" == "y" ]] && /sbin/reboot;
