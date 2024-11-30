38c5ca763178ae73639c39b50346fc32f9714405
Timur <48119843+timamus@users.noreply.github.com>
2024-11-30 08:01:29 +0500

    Update 2_install_system_apps.sh

--- 2_install_system_apps.sh
+++ 2_install_system_apps.sh
@@ -6,9 +6,5 @@ set -Eeuo pipefail
 echo -en "\033[1;33m Installing dconf-editor... \033[0m \n"
 sudo apt install -y dconf-editor
 
-# Installing pavucontrol
-echo -en "\033[1;33m Installing pavucontrol... \033[0m \n"
-sudo apt install -y pavucontrol
-
 echo -en "\033[0;35m Installation successfull \033[0m \n"
 echo 'A system reboot is recommended. Reboot? (y/n)' && read x && [[ "$x" == "y" ]] && /sbin/reboot;
